e7e8fa0795e9f368b3a49d68469501a87a16894f
phamngocanh19127098 <npham4533@gmail.com>
2023-02-13 14:14:14 +0700

    add admin and member user

---
+++ user-auth/V1.2_add_user_role.sql
@@ -0,0 +1,5 @@
+insert into public.roles (id, code, name, description, is_active, is_deleted, is_default)
+ values (8, 'MEMBER', 'Người dùng', 'Người dùng ban đầu', true, false, true );
+
+insert into public.roles (id, code, name, description, is_active, is_deleted, is_default)
+ values (1, 'ADMIN', 'Quản trị viên', 'Quản trị viên hệ thống', true, false, true );
