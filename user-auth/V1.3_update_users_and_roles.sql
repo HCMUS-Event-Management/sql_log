dd41d6307458bb29279b335c9b4c0a459a5b28fc
Nguyen Thanh Vu <thanhvvu34@gmail.com>
2023-02-15 08:51:43 +0700

    Update V1.3_update_users_and_roles.sql

--- user-auth/V1.3_update_users_and_roles.sql
+++ user-auth/V1.3_update_users_and_roles.sql
@@ -3,6 +3,7 @@ ALTER TABLE public.users DROP COLUMN IF EXISTS uuid;
 ALTER TABLE public.users RENAME COLUMN profile_image TO avatar;
 ALTER TABLE public.users DROP COLUMN IF_EXISTS is_forgot_password;
 ALTER TABLE public.users ALTER COLUMN created_on SET DEFAULT now();
+ALTER TABLE public.users RENAME COLUMN created_on TO created_at;
 
 -- Update 'roles'
 ALTER TABLE public.roles DROP COLUMN IF EXISTS is_deleted;
