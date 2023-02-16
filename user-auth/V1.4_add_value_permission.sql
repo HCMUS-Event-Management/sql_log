4454860b949bde79f0dcc72d695eea0190794e85
phamngocanh19127098 <npham4533@gmail.com>
2023-02-16 11:09:50 +0700

    add new value for permission column

---
+++ user-auth/V1.4_add_value_permission.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.permissions (id, code, description, is_active, name) VALUES ('XOA_NGUOI_DUNG', 'Quản lý người dùng', true, 'Xóa');
+INSERT INTO public.permissions (id, code, description, is_active, name) VALUES ('THEM_NGUOI_DUNG', 'Quản lý người dùng', true, 'Thêm');
+INSERT INTO public.permissions (id, code, description, is_active, name) VALUES ('SUA_NGUOI_DUNG', 'Quản lý người dùng', true, 'Sửa');
+
+INSERT INTO public.permissions (id, code, description, is_active, name) VALUES ('XOA_SU_KIEN', 'Quản lý sự kiện', true, 'Xóa');
+INSERT INTO public.permissions (id, code, description, is_active, name) VALUES ('THEM_SU_KIEN', 'Quản lý sự kiện', true, 'Thêm');
+INSERT INTO public.permissions (id, code, description, is_active, name) VALUES ('SUA_SU_KIEN', 'Quản lý sự kiện', true, 'Sửa');
+
