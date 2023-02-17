7f66775096970789437202cd276c8500c5873264
phamngocanh19127098 <npham4533@gmail.com>
2023-02-17 17:47:47 +0700

    add data into feature

---
+++ user-auth/V1.6_add_value_feature.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.feature (id, feature_code, feature_name) VALUES (1, 'MANAGE_TICKET', 'Quản lý vé');
+
+INSERT INTO public.feature (id, feature_code, feature_name) VALUES (2, 'MANAGE_USER', 'Quản lý người dùng');
+
+INSERT INTO public.feature (id, feature_code, feature_name) VALUES (3, 'MANAGE_EVENT', 'Quản lý sự kiện');
+
