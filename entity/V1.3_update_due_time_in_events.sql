3918d9ab4ea531624ba70b388aff3b843723ff56
Nguyen Thanh Vu <thanhvvu34@gmail.com>
2023-02-24 22:30:09 +0700

    Change type of due_time

---
+++ entity/V1.3_update_due_time_in_events.sql
@@ -0,0 +1,11 @@
+-- Create a temporary TIMESTAMP column
+alter table public.events add column create_time_holder timestamp without time zone null;
+
+-- Copy casted value over to the temporary column
+update public.events set create_time_holder = create_time_holder::timestamp;
+
+-- Modify original column using the temporary column
+alter table public.events alter column due_time type timestamp without time zone using create_time_holder;
+
+-- Drop the temporary column (after examining altered column values)
+alter table public.events drop column create_time_holder;
