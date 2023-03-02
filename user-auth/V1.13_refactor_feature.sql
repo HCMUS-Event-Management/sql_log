d0d4c5517c975b5cbb81716acfce558162786ac1
phamngocanh19127098 <npham4533@gmail.com>
2023-03-02 14:17:43 +0700

    refactor feature table

---
+++ user-auth/V1.13_refactor_feature.sql
@@ -0,0 +1,13 @@
+DELETE FROM feature WHERE feature_code = 'MANAGE_TICKET' or feature_code = 'MANAGE_USER' or feature_code = 'MANAGE_EVENT';
+
+ALTER TABLE feature
+    ADD IF NOT EXISTS category varchar(255);
+
+ALTER TABLE feature
+    ADD IF NOT EXISTS sub_category varchar(255);
+
+INSERT INTO public.feature (id, feature_code, feature_name, category, sub_category)
+    VALUES (1, 'rbacRole', 'Manage Roles', 'Entity Management', null);
+
+INSERT INTO public.feature (id, feature_code, feature_name, category, sub_category)
+    VALUES (2, 'user', 'Manage Event Users', 'Entity Management', null);
