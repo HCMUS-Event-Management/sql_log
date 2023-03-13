0ad11371c94db10a46e4678f6fd6e707563a7684
phamngocanh19127098 <npham4533@gmail.com>
2023-03-13 13:04:00 +0700

    create event session

--- entity/V1.6_add_new_table_proposal_ticket.sql
+++ entity/V1.6_add_new_table_proposal_ticket.sql
@@ -1,15 +1,14 @@
+
 CREATE TABLE IF NOT EXISTS proposal_session_ticket
 (
     id           BIGSERIAL PRIMARY KEY,
     ticket_title text,
     "session_id" BIGINT NOT NULL,
-    "event_id"   BIGINT NOT NULL,
     start_time_for_sell     TIMESTAMP,
     end_time_for_sell      TIMESTAMP,
     type         varchar(50),
     price        double precision default 0,
     quantity     BIGINT default 0,
     minimum_ticket_purchased bigint default 1,
-    FOREIGN KEY (session_id) REFERENCES session (id),
-    FOREIGN KEY (event_id) REFERENCES events (id)
+    FOREIGN KEY (session_id) REFERENCES session (id)
 );
