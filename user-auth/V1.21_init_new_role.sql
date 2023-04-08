6c1d0e62e92e7d291b5add43f49f86cae0bb13f6
phamngocanh19127098 <npham4533@gmail.com>
2023-04-08 12:05:35 +0700

    init new feature admin page

---
+++ user-auth/V1.21_init_new_role.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.feature (id , feature_code, feature_name, category, sub_category)
+VALUES (3,'dashboard', 'Dashboard', 'Dashboard', null);
+
+INSERT INTO public.feature (id , feature_code, feature_name, category, sub_category)
+VALUES (4,'revenue', 'Revenue', 'Revenue', null);
