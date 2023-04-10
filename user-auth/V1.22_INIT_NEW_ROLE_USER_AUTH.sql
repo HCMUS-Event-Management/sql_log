1aab67f288f16082dd49108ed592d4c26d64802d
phamngocanh19127098 <npham4533@gmail.com>
2023-04-10 22:14:16 +0700

    add new default value user-auth service

---
+++ user-auth/V1.22_INIT_NEW_ROLE_USER_AUTH.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.feature (id , feature_code, feature_name, category, sub_category)
+VALUES (5,'products', 'Manage Products', 'Entity Management', null);
+
+INSERT INTO public.feature (id , feature_code, feature_name, category, sub_category)
+VALUES (6,'livestreamsetting', 'Livestream Setting', 'Entity Management', null);
