1cf4ff516a5c2d744da4caccfe611242d68197a2
Nguyen Thanh Vu <thanhvvu34@gmail.com>
2023-06-05 15:50:53 +0700

    Add default value for login method

--- user-auth/V1.24_add_login_method.sql
+++ user-auth/V1.24_add_login_method.sql
@@ -1,3 +1,5 @@
 ALTER TABLE public.users ADD login_method varchar(100) NULL;
+ALTER TABLE public.users ALTER COLUMN login_method SET DEFAULT 'basic';
+
 ALTER TABLE public.users ALTER COLUMN birthday DROP NOT NULL;
 ALTER TABLE public.users ALTER COLUMN "password" DROP NOT NULL;
